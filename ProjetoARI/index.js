97a410d05be738bc1da31ee12dbeb14b6eec0ddb
Your Name <you@example.com>
2022-05-19 16:33:56 -0300

    verificações feitas(tirando cpf)

--- ProjetoARI/index.js
+++ ProjetoARI/index.js
@@ -1,55 +1,93 @@
 const form = document.getElementById('form')
 const username = document.getElementById('username')
 const email = document.getElementById('email')
-const senha = document.getElementById('senha')
-const senha2 = document.getElementById('senha2')
+const password = document.getElementById('password')
+const passwordtwo = document.getElementById('password-two')
 const CPF = document.getElementById('CPF')
-const telefone = document.getElementById('telefone')
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     checkInputs()
 })
-function checkInputs() 
+
+function checkInputs() {
 
     const usernameValue = username.value.trim()
     const emailValue = email.value.trim()
-    const senhaValue = senha.value.trim()
-    const senha2Value = senha2.value.trim()
+    const passwordValue = password.value.trim()
+    const passwordtwoValue = passwordtwo.value.trim()
     const CPFValue = CPF.value.trim()
-    const telefoneValue = telefone.value.trim()
+    
 
     if(usernameValue === '') {
         // mostrar erro
         // add classe
-        errorValidation(username, "Preencha esse campo")
+        setErrorFor(username, 'Preencha esse campo')
     } else {
         // adicionar a classe de sucesso
-       sucessValidation(username)
+        setSuccessFor(username)
     }
 
-    if(emailValue === ''){
-        errorValidation(email, "preencha esse campo")
+    if(emailValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(email, 'Preencha esse campo')
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Email inválido')
     } else {
-        sucessValidation(email)
+        // adicionar a classe de sucesso
+        setSuccessFor(email)
+    }
+   
+    if(passwordValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(password, 'Preencha esse campo')
+    } else{
+        // adicionar a classe de sucesso
+        setSuccessFor(password)
     }
 
-    if(senhaValue === ''){
-        errorValidation(password, "preencha esse campo")
+    if(passwordtwoValue === '') {
+        // mostrar erro
+        // add classe
+        setErrorFor(passwordtwo, 'Preencha esse campo')
+    } else if(passwordValue !== passwordtwoValue) { 
+        setErrorFor(passwordtwo, 'Senhas não tão iguais')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(passwordtwo)
     }
 
-    function errorValidation(input, message) {
-        const formControl = input.parentElement;
-        const small = formControl.querySelector('small')
-
-        small.innerText = message
-
-        formControl.className = 'form-control error'
+    if(CPFValue === ''){
+        // mostrar erro
+        // add classe
+        setErrorFor(CPF, 'Preencha esse campo')
+    } else{
+        // adicionar a classe de sucesso
+        setSuccessFor(CPF)
     }
 
-    function sucessValidation(input) {
-        const formControl = input.parentElement;
 
-        formControl.className = 'form-control sucess'
-    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+
+    small.innerText = message
+
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control success'
+}
+
+function isEmail(email) {
+    return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
+}
