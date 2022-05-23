32a43a0daf61b28829e626e731e7d1dc09a1da79
Your Name <you@example.com>
2022-05-23 14:20:17 -0300

    cpf com maskara

--- ProjetoARI/index.js
+++ ProjetoARI/index.js
@@ -94,3 +94,12 @@ function setSuccessFor(input) {
 function isEmail(email) {
     return /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(email)
 }
+
+function mascara_cpf() {
+    let cpf = document.getElementById('CPF')
+        if(cpf.value.length == 3 || cpf.value.length == 7) {
+            cpf.value += "."
+        } else if (cpf.value.length == 11) {
+            cpf.value += "-"
+        }
+}
